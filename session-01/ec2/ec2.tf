7f686ed5d687c757cf74c29209e469e41d7c51f7
Kalyan Reddy <kalyanreddy5030@gmail.com>
2024-01-02 22:24:07 +0530

    first terraform

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,5 +1,14 @@
+#resource "aws_instance" "web" {
+#  ami = "ami-03265a0778a880afb" #Devops-practice
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "HelloTerraform"
+#  }
+#}
+
 resource "aws_instance" "web" {
-  ami = "ami-03265a0778a880afb" #Devops-practice
+  ami           = "ami-03265a0778a880afb" #Devops-practice
   instance_type = "t3.micro"
 
   tags = {
