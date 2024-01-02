82e8e7de9ad2880f31f158250413ec2dad43df28
Kalyan Reddy <kalyanreddy5030@gmail.com>
2024-01-02 22:07:56 +0530

    first terraform

--- session-01/ec2/provider.tf
+++ session-01/ec2/provider.tf
@@ -1,14 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws" # AWS provider version not terraform
-      version = "5.31.0"
+      source = "hashicorp/aws"
+      version = "5.31.0" # AWS provider version, not terraform version
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
 }
 #NOTE: By this provider file terraform will use the aws cli and fetch the access information.
