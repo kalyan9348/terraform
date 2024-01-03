e0cb78332a8b1962464577733ac7c5ffee959efc
Kalyan Reddy <kalyanreddy5030@gmail.com>
2024-01-03 22:51:47 +0530

    security group

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb" #devops-practice
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
-
+#the above command given because we are adding security group for our instance.
   tags = {
     Name = "HelloTerraform"
   }
