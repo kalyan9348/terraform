9a4ba98f904a9a43adf3beee344e0a5f0c29befe
Kalyan Reddy <kalyanreddy5030@gmail.com>
2024-01-03 18:43:29 +0530

    first terraform

--- session-01/ec2/provider.tf
+++ session-01/ec2/provider.tf
@@ -9,5 +9,7 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+#HERE WE CAN GIVE DIRECTLY ACCESS KEY   SECRET KEY BUT NOT RECOMMENDED
+  #AFTER CREATING AWS CLI TERRAFORM WILL FIRST CONNECT TO PROVIDER.TF DATA FROM THERE IT WILL CONNECT TO AWS CLI AND WILL FETCH THE INFORMATION
 }
 #NOTE: By this provider file terraform will use the aws cli and fetch the access information.
