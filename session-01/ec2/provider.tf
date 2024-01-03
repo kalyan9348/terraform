a915c87966fdfa94a921061865ca988b4737602a
Kalyan Reddy <kalyanreddy5030@gmail.com>
2024-01-03 22:42:10 +0530

    security group

--- session-01/ec2/provider.tf
+++ session-01/ec2/provider.tf
@@ -1,3 +1,20 @@
+#terraform {
+#  required_providers {
+#    aws = {
+#      source = "hashicorp/aws"
+#      version = "5.31.0" # AWS provider version, not terraform version
+#    }
+#  }
+#}
+#
+#provider "aws" {
+#  region = "us-east-1"
+#HERE WE CAN GIVE DIRECTLY ACCESS KEY   SECRET KEY BUT NOT RECOMMENDED
+  #AFTER CREATING AWS CLI TERRAFORM WILL FIRST CONNECT TO PROVIDER.TF DATA FROM THERE IT WILL CONNECT TO AWS CLI AND WILL FETCH THE INFORMATION
+#}
+
+#NOTE: By this provider file terraform will use the aws cli and fetch the access information.
+
 terraform {
   required_providers {
     aws = {
@@ -9,7 +26,4 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-#HERE WE CAN GIVE DIRECTLY ACCESS KEY   SECRET KEY BUT NOT RECOMMENDED
-  #AFTER CREATING AWS CLI TERRAFORM WILL FIRST CONNECT TO PROVIDER.TF DATA FROM THERE IT WILL CONNECT TO AWS CLI AND WILL FETCH THE INFORMATION
 }
-#NOTE: By this provider file terraform will use the aws cli and fetch the access information.
