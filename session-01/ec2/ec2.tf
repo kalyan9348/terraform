76044ba4367783886b492aebcf6d2372e8770fee
Kalyan Reddy <kalyanreddy5030@gmail.com>
2024-01-03 22:22:24 +0530

    security group

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -19,3 +19,30 @@ resource "aws_instance" "web" {
     Name = "web"
   }
 }
+
+resource "aws_security_group" "roboshop-all" {   #this is terraform name, for terraform reference only
+  name        = "roboshop-all-aws"  # this is for AWS
+  description = "Allow TLS inbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "Allow all ports"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-all-aws"
+  }
+}
