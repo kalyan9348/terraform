06ab88e669b76518370f08ff460f30fd51cb6993
Kalyan Reddy <kalyanreddy5030@gmail.com>
2024-01-03 13:07:52 +0530

    first terraform

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -7,11 +7,15 @@
 #  }
 #}
 
+#sudo yum install -y yum-utils
+#sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+#sudo yum -y install terraform
+#
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb" #Devops-practice
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloTerraform"
+    Name = "web"
   }
 }
