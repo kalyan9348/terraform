023be6ffd2702efa10b8698a5b47fd4c399ef219
Kalyan Reddy <kalyanreddy5030@gmail.com>
2024-01-05 10:15:18 +0530

    security group

--- variables/variables.tf
+++ variables/variables.tf
@@ -2,14 +2,14 @@
 # 2. -var-file
 # 3. terraform.tfvars
 # 4. ENV variables
-
 variable "ami_id" {
   type = string
   default = "ami-03265a0778a880afb"
 }
-variable instance_type {
-  type = string
+
+variable "instance_type" {
   default = "t2.micro"
+  type = string
 }
 
 variable "tags" {
